4587cd3c3213f6d14c1224984214f226ca355f32
Asif Mohammad Mollah <asif.mollah@gmail.com>
2018-08-12 21:42:32 +0530

    admin_panel: role and user edit completed

--- src/main/java/com/spring_demo/controllers/AdminController.java
+++ src/main/java/com/spring_demo/controllers/AdminController.java
@@ -1,20 +1,24 @@
 package com.spring_demo.controllers;
 
+import com.spring_demo.models.Role;
 import com.spring_demo.models.User;
+import com.spring_demo.repositories.RoleRepository;
 import com.spring_demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import javax.websocket.server.PathParam;
+import java.util.*;
 
 @RestController
 public class AdminController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @GetMapping("/admins")
     public ModelAndView index(Model model){
@@ -27,10 +31,40 @@ public class AdminController {
     }
 
     @GetMapping("/admins/user/{id}/edit")
-    public ModelAndView userEdit(Model model){
-
+    public ModelAndView userEdit(Model model, @PathVariable("id") int id){
+        User user=userRepository.findByUserId(id).get();
+        List<Role> roles=roleRepository.findAll();
+        Collections.sort(roles, new Comparator<Role>() {
+            @Override
+            public int compare(Role role1, Role role2) {
+                return role2.getRoleId()-role1.getRoleId();
+            }
+        });
+        model.addAttribute("user",user);
+        model.addAttribute("roles",roles);
         ModelAndView mav=new ModelAndView();
         mav.setViewName("admins/user_edit");
+        return mav;
+    }
+
+    @PatchMapping("/admins/user/{id}/update")
+    public ModelAndView userUpdate(Model model, @PathVariable("id") int id,
+                                    @RequestParam(value = "first_name", required = true) String fistName,
+                                    @RequestParam(value = "last_name", required = true) String lastName,
+                                    @RequestParam(value = "role", required = true) int roleId,
+                                    @RequestParam(value = "active", required = true) int active
+                                   ){
+        User user=userRepository.findByUserId(id).get();
+        user.setFirstName(fistName);
+        user.setLastName(lastName);
+        user.setActive(active);
+        Role role=roleRepository.findByRoleId(roleId).get();
+        Set<Role> roles=new HashSet<>();
+        roles.add(role);
+        user.setRoles(roles);
+        userRepository.save(user);
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("redirect:/admins/user/"+id+"/edit");
         return mav;
     }
 
--- src/main/java/com/spring_demo/repositories/UserRepository.java
+++ src/main/java/com/spring_demo/repositories/UserRepository.java
@@ -4,9 +4,11 @@ package com.spring_demo.repositories;
 import com.spring_demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.net.CookieStore;
 import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByUserId(int userId);
 }
